78c675d656d59c91f4b9fab2d62cb47c13881a03
Brian Rutledge <bhrutledge@gmail.com>
2013-07-24 23:45:12 -0400

    Publishing to bhrutledge.com

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://bhrutledge.debugged.org'
+SITEURL = 'http://bhrutledge.com'
 RELATIVE_URLS = False
 
 FEED_ATOM = 'feeds/posts.xml'
