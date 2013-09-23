15b85ff46ecbf33622cb0c93ff6dfafdd10678b9
Brian Rutledge <bhrutledge@gmail.com>
2013-09-23 03:45:19 +0000

    Cleaned up tasks

--- fabfile.py
+++ fabfile.py
@@ -2,10 +2,16 @@ from fabric.api import *
 import os
 
 
-env.pelican_input = os.path.expanduser('~/Dropbox/bhrutledge.com/content')
-env.pelican_output = os.path.expanduser('~/Sites/bhrutledge')
-env.pelican_conf = 'pelicanconf.py'
-env.pelican_args = ''
+def dev():
+    env.pelican_input = os.path.expanduser('~/Dropbox/bhrutledge.com/content')
+    env.pelican_output = os.path.expanduser('~/Sites/bhrutledge')
+    env.pelican_conf = 'pelicanconf.py'
+
+
+def live():
+    env.pelican_input = os.path.expanduser('~/Dropbox/bhrutledge.com/content')
+    env.pelican_output = os.path.expanduser('~/webapps/bhrutledge')
+    env.pelican_conf = 'publishconf.py'
 
 
 def clean():
@@ -13,27 +19,14 @@ def clean():
         local('find {pelican_output} -mindepth 1 -delete'.format(**env))
 
 
-def build():
+def build(pelican_args=''):
+    clean()
+    env.pelican_args = pelican_args
     local('pelican {pelican_input} -o {pelican_output} -s {pelican_conf} '
           '{pelican_args}'.format(**env))
-
-
-def rebuild():
-    clean()
-    build()
-
-
-def regenerate():
-    with settings(pelican_args='-r'):
-        rebuild()
-
-
-def publish():
-    with settings(pelican_conf='publishconf.py'):
-        rebuild()
     local('chmod -R a+rX {pelican_output}'.format(**env))
 
 
-def live():
-    env.pelican_output = os.path.expanduser('~/webapps/pelican')
+def regen():
+    build('-r')
 
