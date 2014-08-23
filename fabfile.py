a738ab4f16905ebd15a68ad4f9732a89417e2aed
Brian Rutledge <bhrutledge@gmail.com>
2014-08-22 21:59:23 -0400

    Change resuem location

--- fabfile.py
+++ fabfile.py
@@ -2,21 +2,20 @@ from fabric.api import *
 import os
 
 # TODO: Switch to Invoke
+# TODO: Move content back to Git?
+
 
 def dev():
     env.pelican_input = os.path.expanduser('~/Dropbox/bhrutledge.com/content')
     env.pelican_output = os.path.expanduser('~/Sites/bhrutledge')
+    env.resume_dir = os.path.expanduser('~/Code/resume')
     env.pelican_conf = 'pelicanconf.py'
-    env.resume = os.path.expanduser(
-        '~/Dropbox/bhrutledge.com/resume/output/resume.html')
 
 
 def live():
-    env.pelican_input = os.path.expanduser('~/Dropbox/bhrutledge.com/content')
+    dev()
     env.pelican_output = os.path.expanduser('~/webapps/bhrutledge')
     env.pelican_conf = 'publishconf.py'
-    env.resume = os.path.expanduser(
-        '~/Dropbox/bhrutledge.com/resume/output/resume.html')
 
 
 def clean():
@@ -24,12 +23,18 @@ def clean():
         local('find {pelican_output} -mindepth 1 -delete'.format(**env))
 
 
+def resume():
+    local('cp {resume_dir}/brian-rutledge-resume.html '
+          '{pelican_output}/resume.html'.format(**env))
+    local('cp {resume_dir}/resume.css {pelican_output}'.format(**env))
+
+
 def build(pelican_args=''):
     clean()
     env.pelican_args = pelican_args
     local('pelican {pelican_input} -o {pelican_output} -s {pelican_conf} '
           '{pelican_args}'.format(**env))
-    local('cp {resume} {pelican_output}'.format(**env))
+    resume()
     local('chmod -R a+rX {pelican_output}'.format(**env))
 
 
