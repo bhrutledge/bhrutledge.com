f7d548d591cee1af8a9f67502742adaafe6b316e
Brian Rutledge <bhrutledge@gmail.com>
2014-08-24 23:39:55 +0000

    Don't copy resume

--- fabfile.py
+++ fabfile.py
@@ -8,7 +8,6 @@ import os
 def dev():
     env.pelican_input = os.path.expanduser('~/Dropbox/bhrutledge.com/content')
     env.pelican_output = os.path.expanduser('~/Sites/bhrutledge')
-    env.resume_dir = os.path.expanduser('~/Code/resume')
     env.pelican_conf = 'pelicanconf.py'
 
 
@@ -23,18 +22,11 @@ def clean():
         local('find {pelican_output} -mindepth 1 -delete'.format(**env))
 
 
-def resume():
-    local('cp {resume_dir}/brian-rutledge-resume.html '
-          '{pelican_output}/resume.html'.format(**env))
-    local('cp {resume_dir}/resume.css {pelican_output}'.format(**env))
-
-
 def build(pelican_args=''):
     clean()
     env.pelican_args = pelican_args
     local('pelican {pelican_input} -o {pelican_output} -s {pelican_conf} '
           '{pelican_args}'.format(**env))
-    resume()
     local('chmod -R a+rX {pelican_output}'.format(**env))
 
 
